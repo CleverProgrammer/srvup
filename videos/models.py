e95318fae7eb0efe81a90a8e91c36e4b601ad39d
rafeh01 <rafehqazi1@gmail.com>
2016-06-17 01:31:29 -0500

    Change embed_code max length to 500 from 120 and add video emded code in html

--- videos/models.py
+++ videos/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Video(models.Model):
     title = models.CharField(max_length=120)
-    embed_code = models.CharField(max_length=120, blank=True, null=True)
+    embed_code = models.CharField(max_length=500, blank=True, null=True)
 
     def __str__(self):
         return self.title
