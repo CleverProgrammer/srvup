b43f1bd703e12c90ff722e48d66a119246dd1172
rafeh01 <rafehqazi1@gmail.com>
2016-06-17 01:54:05 -0500

    Use marksafe to render youtube videos on front page

--- srvup/views.py
+++ srvup/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
-
+from django.utils.safestring import mark_safe
 from videos.models import Video
 
 
 def home(request):
     name = 'Rafeh Qazi'
     videos = Video.objects.all()
+    embeds = [(mark_safe(video.embed_code)) for video in videos]
+    print(isinstance(embeds[0], str))
     context = {
         'name': name,
         'number': videos.count(),
         'videos': videos,
+        'embeds': embeds,
     }
     return render(request, 'home.html', context)
