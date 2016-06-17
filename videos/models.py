f4238a8a1755b8f4c47f58b8388c029b4728de5a
QaziPython <rafehqazi1@gmail.com>
2016-06-17 12:38:38 -0500

    Add active, featured, and free preview boolean fields to models

--- videos/models.py
+++ videos/models.py
@@ -5,9 +5,12 @@ from django.core.urlresolvers import reverse
 class Video(models.Model):
     title = models.CharField(max_length=120)
     embed_code = models.CharField(max_length=500, blank=True, null=True)
+    active = models.BooleanField(default=True)
+    featured = models.BooleanField(default=False)
+    free_preview = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
-        return reverse(viewname='video_detail', args=[str(self.id)])
+        return reverse(viewname='video_detail', kwargs={'id': self.id})
