032eb3a028aa00e850d6eae8dbede92237ff14ae
QaziPython <rafehqazi1@gmail.com>
2016-06-17 12:10:47 -0500

    Add get_absolute_url to get a url to each specific video

--- videos/models.py
+++ videos/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 class Video(models.Model):
@@ -7,3 +8,6 @@ class Video(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse(viewname='video_detail', args=[str(self.id)])
