b8e90fef330916c19fef9b52213691e2eb859ad0
rafeh01 <rafehqazi1@gmail.com>
2016-06-19 02:30:09 -0500

    Start using aws postgres database instead of sqlite3

--- srvup/settings.py
+++ srvup/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 
+from .database_config import config
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -92,8 +94,12 @@ WSGI_APPLICATION = 'srvup.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME'  : os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME'  : config['name'],
+        'USER': config['user'],
+        'PASSWORD': config['password'],
+        'HOST': config['host'],
+        'PORT': config['port']
     }
 }
 
