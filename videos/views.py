40378b059a8be0d040baae6dcccbe847f6d767ff
rafeh01 <rafehqazi1@gmail.com>
2016-06-19 07:41:39 -0500

    Add comment form for each video

--- videos/views.py
+++ videos/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, Http404
 from django.contrib.auth.decorators import login_required
 
 from comments.models import Comment
+from comments.forms import CommentForm
 
 from .models import Video, Category
 
@@ -20,7 +21,15 @@ def video_detail(request, cat_slug, vid_slug):
         video = Video.objects.get(slug=vid_slug)
         context['object'] = video
         context['comments'] = video.comment_set.all()
+        comment_form = CommentForm(request.POST or None)
+        if comment_form.is_valid():
+            obj_instance = comment_form.save(commit=False)
+            obj_instance.path = request.get_full_path()
+            obj_instance.user = request.user
+            obj_instance.save()
+            return render(request, 'videos/video_detail.html', context)
         # context['comments'] = Comment.objects.filter(video=context['object'])
+        context['comment_form'] = comment_form
         return render(request, 'videos/video_detail.html', context)
     except:
         raise Http404
